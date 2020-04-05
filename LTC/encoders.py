df8db141592c921e93e72d6d4341486db34e29e0
Edgar RP <jefelitman@gmail.com>
2020-04-05 07:48:31 -0500

    LTC_running_faster

--- LTC/encoders.py
+++ LTC/encoders.py
@@ -13,8 +13,17 @@ def basic_encoder(inputs, cell_units, n_layer, with_sequences):
 
 def unique_bidirectional_encoder(inputs, cell_units, n_layer, with_sequences):
     x = reshape_layer(inputs, n_layer)
-    return keras.layer.Bidirectional(keras.layers.LSTM(units=cell_units,
+    return keras.layers.Bidirectional(keras.layers.LSTM(units=cell_units,
                                                       return_sequences=with_sequences),
                                      merge_mode='concat',
                                      name="Bi-LSTM-"+str(cell_units)+"_"+str(n_layer)
                                     )(x)
+
+def jota_encoder_slt(inputs, cell_units, n_layer, with_sequences):
+    x = reshape_layer(inputs, n_layer)
+    x, h1, c1 = keras.layers.LSTM(units = cell_units, return_sequences = True,
+                            return_state=True,
+                            name = "LTSM1-"+str(cell_units)+"_"+str(n_layer))(x)
+    x = keras.layers.LSTM(units = cell_units, return_sequences = with_sequences,
+                            name = "LTSM2-"+str(cell_units)+"_"+str(n_layer))(x, initial_state=[h1,c1])
+    return x
