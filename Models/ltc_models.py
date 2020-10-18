a89789fa10afb8a405e22c5d85a0836af275fbc8
Edgar RP <jefelitman@gmail.com>
2020-10-18 21:03:39 +0000

    Unnamed changes

--- Models/ltc_models.py
+++ Models/ltc_models.py
@@ -1,13 +1,33 @@
+"""This file contain the models server to generate the answer to the client
+Version: 1.9.4
+Made by: Yesid Gutierrez and Edgar Rangel
+"""
 from tensorflow import keras
-from inception_modules import *
-from encoders import *
+from .inception_modules import *
+from .encoders_models import *
 
-def get_convs_blocks5(inputs, weigh_decay):
+def get_5_blocks_conv(first_block_input:keras.layers.Layer, with_batch_norm:bool = False, 
+    layers_padding:str = "same", layers_activation:str = "relu",
+    with_bias:bool = True, regularizers:tuple = (None, None), initializers:tuple = (None, None)):
+    """Function that return the tensorflow graph (forward pipeline) with the parameters given. This 
+    method apply 5 convolutional blocks to the current forward pipeline (first_block_input) and return 
+    the output of last block. Finally the BatchNormalization layers are by default and no customizable.
+    Args:
+        first_block_input: An instance of tf.keras.layers.Layer with the input to the first block.
+        with_batch_norm: A boolean indicating if apply BatchNormalization after the convolutional layer.
+        layers_padding: String indicating how to do the padding in convolutional layers.
+        layers_activation: String indicating what activation must be applied to convolutional layers.
+        with_bias: A boolean indicating if the convolutional layers can have bias.
+        regularizers: A tuple with two tf.keras.regularizers instances, the structure is (kernel_regularizer, 
+            bias_regularizer). This apply over all the convolutional layers.
+        initializers: A tuple with two tf.keras.initializers instances, the structure is (kernel_initializer, 
+            bias_initializer). This apply over all the convolutional layers.
+    """
     #Conv1
     x = keras.layers.Conv3D(filters=64, kernel_size=3, padding="same", activation="relu", 
                               kernel_regularizer=keras.regularizers.l2(weigh_decay),
-                              name='conv3d_1')(inputs)
-    x = keras.layers.BatchNormalization(axis=4, name="batch_norm_1")(x)
+                              name='conv3d_1')(first_block_input)
+    x = keras.layers.BatchNormalization(name="batch_norm_1")(x)
     x = keras.layers.MaxPool3D(pool_size=(1,2,2),strides=(1,2,2), name='max_pooling3d_1')(x)
 
     #Conv2
